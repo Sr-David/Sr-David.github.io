5cfdcb489366b86cb504380dc901d3e31111d79e
David_Calvo <david.calvo@mataro.epiaedu.cat>
2024-06-07 17:31:52 +0200

    pagina acabada

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY || document.documentElement.scrollTop;
     var divVideoHeight = document.querySelector('.divVideo').offsetHeight;
-    console.log(scrollPosition);
-    console.log("DIV VIDEO: "+ divVideoHeight);
+
     if (scrollPosition > divVideoHeight - 600) {
         var skills = {
             'java': 60,
@@ -29,3 +28,54 @@ window.addEventListener('scroll', function() {
         })();
     }
 });
+
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    var contenedorEdu = document.querySelector('.contenedorEdu').offsetHeight;
+    
+
+    if (scrollPosition > contenedorEdu + 300) {
+        var bordes = document.querySelectorAll('.bordeEdu');
+        bordes.forEach(function(borde) {
+            borde.style.borderImage = 'linear-gradient(to bottom, orange 100%, transparent 0%) 1';
+        });
+    }
+});
+
+
+
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    var contenedorExp = document.querySelector('.contenedorExp');
+    if (contenedorExp) {
+        var contenedorExpHeight = contenedorExp.offsetHeight;
+        console.log("Contenedor " + contenedorExpHeight);
+        console.log("Scroll position " + scrollPosition);
+
+        if (scrollPosition > contenedorExpHeight + 1000) {
+            var bordes = document.querySelectorAll('.bordeEdu2');
+            bordes.forEach(function(borde) {
+                borde.style.borderImage = 'linear-gradient(to bottom, orange 100%, transparent 0%) 1';
+            });
+        }
+    }
+});
+
+
+
+
+
+
+
+
+function copiarTexto() {
+   
+    navigator.clipboard.writeText("david.calvo.va@gmail.com");
+    alert("Email copied to clipboard:");
+   
+}
+
+
+function irLinkedin(){
+    window.open("https://www.linkedin.com/in/david-calvo-vates-578708270/");
+}
